578c65fd658a3af184b2656e3cc1f2bceddb4cc7
shalini <s.rathore43@gmail.com>
2026-03-13 11:44:21 +0530

    dasd

--- src/components/Refeg.jsx
+++ src/components/Refeg.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { useRef } from 'react'
+import { href } from 'react-router-dom'
 export default function Refeg() {
     let nameref = useRef()
     let pararef = useRef()
+    let aref = useRef()
+    let bref = useRef()
+    let mref = useRef()
     let fun1 = ()=>
     {
         let msg = "welcome "+ nameref.current.value
@@ -12,6 +16,26 @@ export default function Refeg() {
         pararef.current.style.fontSize="25px"
         nameref.current.value = ""
     }
+    function calculate(op)
+    {
+        let a = parseInt(aref.current.value)
+        let b = parseInt(bref.current.value)
+        switch (op) {
+            case '+':
+        mref.current.innerHTML = "Sum is "+ (a+b)        
+                break;
+            case '-':
+                mref.current.innerHTML = "difference is "+ (a-b)
+                break;
+            case '*':
+            mref.current.innerHTML = "Multiplication is "+ (a*b)
+                break;
+            case '/':
+            mref.current.innerHTML = "Division is "+ (a/b)    
+                break;                    
+        }
+      
+    }
   return (
     <div>
         <p>Enter name : <input type="text" ref={nameref} /></p>
@@ -19,13 +43,16 @@ export default function Refeg() {
         <p ref={pararef}></p>
         <hr />
         <p>
-            a: <input type="text" name="" id="" />
+            a: <input type="text" name="" ref={aref}/>
         </p>
         <p>
-            b: <input type="text" />
+            b: <input type="text" ref={bref} />
         </p>
-        <input type="button" value="+" />
-        <h2>Show result here</h2>
+        <input type="button" value="+" onClick={()=>calculate('+')} />
+        <input type="button" value="-" onClick={()=>calculate('-')} />
+        <input type="button" value="*" onClick={()=>calculate('*')} />
+        <input type="button" value="/" onClick={()=>calculate('/')} />
+        <h2 ref={mref}></h2>
     </div>
   )
 }
